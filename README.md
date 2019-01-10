233211f2f170804b25c50c9e463ddbf7bcbddb6c
Lewis Gregerson-Spencer <lewisgregerson-spencer@Lewiss-MacBook.local>
2019-01-10 13:14:11 -0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Welcom to the Office Clicky Game!
-
-Try to click on all the employees once, but don't click the same one twice!
---
+++ precache-manifest.e03bbb716cd19ab9397e66e3ae45481b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b751c1d616b86443175",
+    "url": "/lewisgs22/clicky-game/static/js/runtime~main.3b751c1d.js"
+  },
+  {
+    "revision": "2492e4a02d12949a61d3",
+    "url": "/lewisgs22/clicky-game/static/js/main.2492e4a0.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/lewisgs22/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "2492e4a02d12949a61d3",
+    "url": "/lewisgs22/clicky-game/static/css/main.30ac5cc0.chunk.css"
+  },
+  {
+    "revision": "362059c39ea388cf5fa898b56505f5d8",
+    "url": "/lewisgs22/clicky-game/index.html"
+  }
+];
